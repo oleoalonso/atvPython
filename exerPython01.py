683cd685d2f841ad2e026c59ae18b2844f9b703b
Leonardo <oleoalonso@gmail.com>
2021-09-24 15:26:08 -0300

    Exercício Prático Python 01

---
+++ exerPython01.py
@@ -0,0 +1,12 @@
+# Leonardo L. Alonso Carriço
+# leonardo@carrico.com.br
+
+# Exercício Prático Python 01
+
+###################################################################
+# 1) Faça um Programa que converta metros para centímetros.
+
+metros = int(input("\nEntre com um número em metros : "))
+centimetros = metros * 100;
+
+print(f"\n{metros}m convertido para centimetros é: {centimetros}\n")
